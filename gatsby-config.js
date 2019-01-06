38846307b925f5e729d508c202473bf1ae836828
Yagiz Nizipli <yagiznizipli@me.com>
2019-01-07 00:53:09 +0300

    feat(analytics): Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-131828554-1`,
       },
     },
     `gatsby-plugin-feed`,
