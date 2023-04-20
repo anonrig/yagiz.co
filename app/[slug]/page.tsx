ed8c67be9b9595ca8ac3321bac6c037590309779
Yagiz Nizipli <yagiz@nizipli.com>
2023-04-20 17:34:03 -0400

    add twitter card style for blog posts

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -38,6 +38,7 @@ export function generateMetadata({ params }: Props): Metadata {
       tags: blog.tag ? [blog.tag.title] : [],
     },
     twitter: {
+      card: 'summary_large_image',
       title: blog.title,
       description: blog.description,
     },
