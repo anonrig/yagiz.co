3942543c723428420857f07336b03235c6fe2dcb
Yagiz Nizipli <yagiz@nizipli.com>
2023-04-24 11:46:50 -0400

    convert most pages to static (#4)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next'
 
 import Page, { getPageMetadata } from '@/components/page'
 
+// Force static rendering and static data fetching of a layout or page
+export const dynamic = 'force-static'
+
 export const metadata: Metadata = getPageMetadata({ slug: 'about' })
 
 export default function About() {
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,6 +3,9 @@ import { Metadata } from 'next'
 import ContactForm from '@/components/contact-form'
 import Page, { getPageMetadata } from '@/components/page'
 
+// Force static rendering and static data fetching of a layout or page
+export const dynamic = 'force-static'
+
 export const metadata: Metadata = getPageMetadata({ slug: 'contact' })
 
 export default function Press() {
