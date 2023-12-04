de387b10853bd7c381a41676bdd126876e40faa8
Yagiz Nizipli <yagiz@nizipli.com>
2023-12-04 17:04:53 -0500

    remove security headers

--- next.config.js
+++ next.config.js
@@ -7,14 +7,6 @@ const plugins = [withContentlayer]
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ]
-  },
   redirects() {
     return [{ source: '/rss.xml', destination: '/rss', permanent: true }]
   },
@@ -26,39 +18,6 @@ const nextConfig = {
     optimizePackageImports: ['lucide-react', 'date-fns'],
   },
 }
-
-const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains; preload',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
-  },
-]
 
 module.exports = withSentryConfig(
   plugins.reduce((acc, next) => next(acc), nextConfig),
