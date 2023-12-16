2df32ed0603209357d2cfe406c595b3b15070bd8
Yagiz Nizipli <yagiz@nizipli.com>
2023-12-16 16:28:01 -0500

    fix cloudflare import

--- temp/astro.config.mjs
+++ temp/astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "astrojs/cloudflare"
+import cloudflare from "@astrojs/cloudflare"
 
 // https://astro.build/config
 export default defineConfig({
