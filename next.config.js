13ae5d2dbbf4ca9039df4616fc49e6b448618910
Yagiz Nizipli <yagiz@nizipli.com>
2023-12-25 16:56:04 -0500

    add recap 2023 blog post

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ const nextConfig = {
   },
   images: {
     formats: ['image/avif', 'image/webp'],
+    remotePatterns: [{ protocol: 'https', hostname: 'i3.ytimg.com' }],
   },
   experimental: {
     webpackBuildWorker: true,
