e80a585d41b92b6fec53408fe0a4b088829918cb
Florian Lefebvre <contact@florian-lefebvre.dev>
2023-12-27 10:59:56 +0100

    fix: font error on build

--- temp/src/pages/[slug]/opengraph-image.ts
+++ temp/src/pages/[slug]/opengraph-image.ts
@@ -3,11 +3,15 @@ import type { APIRoute, GetStaticPaths, InferGetStaticPropsType } from "astro";
 import satori from "satori";
 import { Resvg } from "@resvg/resvg-js";
 import { BlogOG } from "@/components/BlogOG";
+import { resolve } from "node:path";
+import { readFile } from "node:fs/promises";
 
 type Props = InferGetStaticPropsType<typeof getStaticPaths>;
 
 const WIDTH = 1200;
 const HEIGHT = 600;
+
+const getFontData = async () => readFile(resolve("./public/fonts/mulish.ttf"));
 
 export const getStaticPaths = (async () => {
   const posts = await getPosts();
@@ -18,14 +22,12 @@ export const getStaticPaths = (async () => {
   }));
 }) satisfies GetStaticPaths;
 
-export const GET: APIRoute<Props> = async ({ props: { post }, url }) => {
+export const GET: APIRoute<Props> = async ({ props: { post } }) => {
   const svg = await satori(BlogOG({ post }), {
     fonts: [
       {
         name: "Mulish Variable",
-        data: await fetch(`${url.origin}/fonts/mulish.ttf`).then((res) =>
-          res.arrayBuffer()
-        ),
+        data: await getFontData(),
         style: "normal",
       },
     ],
