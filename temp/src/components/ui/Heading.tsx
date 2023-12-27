5de73be1a135de53fcddd889db0d89df9d6ef824
Florian Lefebvre <contact@florian-lefebvre.dev>
2023-12-27 12:51:30 +0100

    feat: use astro components whenever possible

--- temp/src/components/ui/Heading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Heading(props: { text: string }) {
-  return (
-    <h1 className="col-main text-4xl font-extrabold tracking-tight text-slate-800 dark:text-white">
-      {props.text}
-    </h1>
-  )
-}
