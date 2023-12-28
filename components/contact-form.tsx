17cc01f46ef5c3ba2b0bbeb66eb67c5440e94bd0
onurravli <onurravli@hotmail.com>
2023-12-28 20:06:41 +0300

    fix: change text colors in contact form to improve accesebility

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -14,7 +14,7 @@ export default function ContactForm(props: Props) {
       className={clsx('grid grid-cols-canvas [&>*]:col-main gap-y-4', props.className)}
     >
       {state.succeeded && !state.submitting && (
-        <div className="bg-[#f6f6f6] dark:bg-[#2f333c] text-neutral-800 px-4 sm:px-6 py-2 rounded-md text-sm flex flex-col sm:flex-row sm:gap-x-2">
+        <div className="bg-[#f6f6f6] dark:bg-[#2f333c] text-neutral-300 px-4 sm:px-6 py-2 rounded-md text-sm flex flex-col sm:flex-row sm:gap-x-2">
           <div className="font-extrabold">Message sent succesfully.</div> I'll respond to your
           message as soon as possible.
         </div>
@@ -93,7 +93,7 @@ export default function ContactForm(props: Props) {
         <Form.Submit
           disabled={state.submitting || state.succeeded}
           className={clsx(
-            'disabled:opacity-60 disabled:cursor-not-allowed uppercase mt-2 items-center bg-white dark:bg-white-reversed border-[1px] border-slate-200 dark:border-neutral-600 hover:border-slate-300 dark:hover:border-neutral-500 border-solid rounded-md text-orange-400 text-[11px] font-extrabold h-[36px] justify-center tracking-[0.5px] outline-none px-[15px]',
+            'disabled:opacity-60 disabled:cursor-not-allowed uppercase mt-2 items-center bg-white dark:bg-white-reversed border-[1px] border-slate-200 dark:border-neutral-600 hover:border-slate-300 dark:hover:border-neutral-500 border-solid rounded-md dark:text-orange-100 text-[11px] font-extrabold h-[36px] justify-center tracking-[0.5px] outline-none px-[15px]',
             {
               'cursor-not-allowed': state.succeeded,
               'cursor-progress': state.submitting,
