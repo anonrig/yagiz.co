bba1e35e4ed40a1703176308887ee6dba77c086e
Florian Lefebvre <contact@florian-lefebvre.dev>
2023-12-28 12:20:46 +0100

    feat: work on cli

--- .github/cli/create-blog.mjs
+++ .github/cli/create-blog.mjs
@@ -1,12 +1,32 @@
-import fs from 'node:fs'
-import path from 'node:path'
+import fs from "node:fs";
+import path from "node:path";
+import { cancel, group, outro, select, spinner, text } from "@clack/prompts";
 
-import { cancel, group, outro, select, spinner, text } from '@clack/prompts'
-import { format } from 'date-fns'
+/**
+ *
+ * @param {Date} date
+ * @returns
+ */
+const format = (date) => {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
 
-import { allBlogs, allTags } from '../../.contentlayer/generated/index.mjs'
+  return `${year}-${month}-${day}`;
+};
 
-const folder = new URL('../../content', import.meta.url).pathname
+const folder = path.resolve("./src/content");
+const allTags = (() => {
+  const dir = path.join(folder, "tags");
+  const filenames = fs.readdirSync(dir);
+  return filenames.map((f) => path.basename(f, path.extname(f)));
+})();
+
+/**
+ *
+ * @param {{ title: string; description: string; tag: string; status: string; }} param0
+ * @returns
+ */
 const getTemplateFor = ({ title, description, tag, status }) =>
   `
 ---
@@ -14,91 +34,90 @@ title: '${title}'
 description: >-
   ${description}
 type: Blog
-date: '${format(new Date(), 'yyyy-MM-dd')}'
+date: '${format(new Date())}'
 tag: ${tag}
 status: ${status}
 ---
-`.trim()
+`.trim();
 
 export async function createBlog() {
   const { title, slug, description, tag, status } = await group(
     {
       title: () =>
         text({
-          message: 'What is the title of your post?',
-          hint: 'Recommended length less than 60 characters.',
+          message: "What is the title of your post?",
+          hint: "Recommended length less than 60 characters.",
           validate(value) {
             if (value.length === 0) {
-              return 'Value is required'
+              return "Value is required";
             }
           },
         }),
       slug: () =>
         text({
-          message: 'Which slug do you want to use?',
+          message: "Which slug do you want to use?",
           required: true,
           validate(value) {
             if (value.length === 0) {
-              return 'Value is required'
+              return "Value is required";
             }
 
-            const file = path.join(folder, `${value}.mdx`)
+            const file = path.join(folder, `blog/${value}.mdx`);
             if (fs.existsSync(file)) {
-              return 'Slug is already used'
+              return "Slug is already used";
             }
 
             // only allow letters, numbers and hypens
-            const re = new RegExp('^[a-z0-9]+(?:-[a-z0-9]+)*$', 'g')
+            const re = new RegExp("^[a-z0-9]+(?:-[a-z0-9]+)*$", "g");
             if (re.exec(value) === null) {
-              return 'Slug should be all lowercase and contain only letters, numbers, and hyphens'
+              return "Slug should be all lowercase and contain only letters, numbers, and hyphens";
             }
           },
         }),
       description: () =>
         text({
-          message: 'What do you want as a description?',
-          defaultValue: '',
+          message: "What do you want as a description?",
+          defaultValue: "",
           required: false,
         }),
       tag: () =>
         select({
-          message: 'Select a tag',
+          message: "Select a tag",
           required: true,
           options: allTags.map((tag) => ({
-            value: `tags/${tag.slug}.mdx`,
-            label: tag.title,
-            hint: `${allBlogs.filter((b) => b.tag.slug === tag.slug).length} blogs written`,
+            value: tag,
+            label: tag,
           })),
         }),
       status: () =>
         select({
-          message: 'Select a status',
+          message: "Select a status",
           required: true,
           options: [
-            { value: 'published', label: 'Published' },
-            { value: 'draft', label: 'Draft' },
+            { value: "published", label: "Published" },
+            { value: "draft", label: "Draft" },
           ],
         }),
     },
     {
       onCancel() {
-        cancel('Operation cancelled')
-        process.exit(0)
+        cancel("Operation cancelled");
+        process.exit(0);
       },
-    },
-  )
+    }
+  );
 
   const template = getTemplateFor({
     title,
     description,
     tag,
     status,
-  })
-  const creating = spinner()
-  creating.start('Creating the document')
-  const documentPath = path.join(folder, `blog/${slug}.mdx`)
-  await fs.promises.writeFile(documentPath, template.trim(), 'utf-8')
-  creating.stop()
+  });
+  const creating = spinner();
+  creating.start("Creating the document");
+  const documentPath = path.join(folder, `blog/${slug}.mdx`);
+  await fs.promises.writeFile(documentPath, template.trim(), "utf-8");
+  creating.stop();
 
-  outro(`Blog post is created at ${documentPath}`)
+  outro(`Blog post is created at ${documentPath}`);
 }
