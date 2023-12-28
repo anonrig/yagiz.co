313c1deb61addce649046405239358a015136d17
Florian Lefebvre <contact@florian-lefebvre.dev>
2023-12-28 17:26:46 +0100

    feat: extract layout constants to content.ts

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -4,6 +4,9 @@ import { getCollection, getEntry } from 'astro:content'
 export const githubImage = 'https://github.com/anonrig.png'
 export const twitterUrl = 'https://twitter.com/yagiznizipli'
 export const rssUrl = 'https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fwww.yagiz.co%2Frss%2F'
+export const DEFAULT_TITLE = 'Engineering with Yagiz'
+export const DEFAULT_DESCRIPTION =
+  "Here's a collection of posts about my thoughts, stories, ideas and experiences as a human, and an engineer working with different technologies."
 
 export const getPosts = async () => {
   const posts = (
