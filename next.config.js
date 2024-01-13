592b10d7a63f0c79b8fb141feaebdcfd80085a60
Yagiz Nizipli <yagiz@nizipli.com>
2024-01-13 16:53:05 -0500

    remove unnecessary experiment

--- next.config.js
+++ next.config.js
@@ -15,7 +15,6 @@ const nextConfig = {
     remotePatterns: [{ protocol: 'https', hostname: 'i3.ytimg.com' }],
   },
   experimental: {
-    outputFileTracing: false,
     optimizePackageImports: ['lucide-react', 'date-fns'],
     serverComponentsExternalPackages: ['@sentry/profiling-node'],
   },
