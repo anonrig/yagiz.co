258b6425f8fe21f8981f05b27be21778d40909ed
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-01-14 13:34:36 +0100

    feat: catch up with main's changes and upgrade dependencies

--- sentry/server.ts
+++ sentry/server.ts
@@ -1,10 +1,8 @@
 import * as Sentry from '@sentry/astro'
-import { ProfilingIntegration } from '@sentry/profiling-node'
 import { SENTRY_DSN } from '../constants'
 
 Sentry.init({
   dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
   debug: false,
-  integrations: [new ProfilingIntegration()],
 })
