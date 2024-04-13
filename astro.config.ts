6ebe74d95f9ffb30f5db37648cbdd27f94c77ab4
Yagiz Nizipli <yagiz@nizipli.com>
2024-04-13 10:18:59 -0400

    remove last remove components

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
 import cloudflare from '@astrojs/cloudflare'
 import mdx from '@astrojs/mdx'
-import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
@@ -23,7 +22,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-    react(),
     mdx({
       syntaxHighlight: false,
       remarkPlugins: [remarkGfm],
